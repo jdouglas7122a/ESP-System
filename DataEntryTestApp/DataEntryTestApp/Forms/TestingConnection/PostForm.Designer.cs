4d8cd2bc612c8f4c5e173ba0e3a72f697af7aa64
Sliferss <60744269+Sliferss@users.noreply.github.com>
2020-04-12 20:35:40 +0100

    POST(INSERT INTO) added
    The Insert command was added, post was modified as it did not ask for the table name which the command needs, there is no table to test with atm but should be working.

--- DataEntryTestApp/DataEntryTestApp/Forms/TestingConnection/PostForm.Designer.cs
+++ DataEntryTestApp/DataEntryTestApp/Forms/TestingConnection/PostForm.Designer.cs
@@ -29,12 +29,14 @@
         private void InitializeComponent()
         {
             this.FormHeader = new System.Windows.Forms.Panel();
-            this.TargetTableBox = new System.Windows.Forms.TextBox();
+            this.TableColumn = new System.Windows.Forms.TextBox();
             this.DataBox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.SubmitButton = new System.Windows.Forms.Button();
             this.CancelButton = new System.Windows.Forms.Button();
+            this.TableName = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // FormHeader
@@ -42,44 +44,50 @@
             this.FormHeader.BackColor = System.Drawing.SystemColors.ControlLight;
             this.FormHeader.Dock = System.Windows.Forms.DockStyle.Top;
             this.FormHeader.Location = new System.Drawing.Point(0, 0);
+            this.FormHeader.Margin = new System.Windows.Forms.Padding(4);
             this.FormHeader.Name = "FormHeader";
-            this.FormHeader.Size = new System.Drawing.Size(304, 46);
+            this.FormHeader.Size = new System.Drawing.Size(405, 57);
             this.FormHeader.TabIndex = 0;
             this.FormHeader.MouseDown += new System.Windows.Forms.MouseEventHandler(this.FormHeader_MouseDown);
             this.FormHeader.MouseMove += new System.Windows.Forms.MouseEventHandler(this.FormHeader_MouseMove);
             this.FormHeader.MouseUp += new System.Windows.Forms.MouseEventHandler(this.FormHeader_MouseUp);
             // 
-            // TargetTableBox
+            // TableColumn
             // 
-            this.TargetTableBox.Location = new System.Drawing.Point(95, 73);
-            this.TargetTableBox.Name = "TargetTableBox";
-            this.TargetTableBox.Size = new System.Drawing.Size(151, 20);
-            this.TargetTableBox.TabIndex = 8;
+            this.TableColumn.Location = new System.Drawing.Point(127, 123);
+            this.TableColumn.Margin = new System.Windows.Forms.Padding(4);
+            this.TableColumn.Name = "TableColumn";
+            this.TableColumn.Size = new System.Drawing.Size(200, 22);
+            this.TableColumn.TabIndex = 8;
             // 
             // DataBox
             // 
-            this.DataBox.Location = new System.Drawing.Point(95, 128);
+            this.DataBox.Location = new System.Drawing.Point(127, 158);
+            this.DataBox.Margin = new System.Windows.Forms.Padding(4);
             this.DataBox.Name = "DataBox";
-            this.DataBox.Size = new System.Drawing.Size(151, 20);
+            this.DataBox.Size = new System.Drawing.Size(200, 22);
             this.DataBox.TabIndex = 9;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.label1.Location = new System.Drawing.Point(41, 76);
+            this.label1.Location = new System.Drawing.Point(27, 128);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.Size = new System.Drawing.Size(97, 17);
             this.label1.TabIndex = 10;
-            this.label1.Text = "Table #:";
+            this.label1.Text = "Table column:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.label2.Location = new System.Drawing.Point(41, 131);
+            this.label2.Location = new System.Drawing.Point(27, 163);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.Size = new System.Drawing.Size(42, 17);
             this.label2.TabIndex = 11;
             this.label2.Text = "Data:";
             // 
@@ -92,9 +100,10 @@
             this.SubmitButton.Font = new System.Drawing.Font("Candara", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.SubmitButton.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
             this.SubmitButton.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.SubmitButton.Location = new System.Drawing.Point(3, 173);
+            this.SubmitButton.Location = new System.Drawing.Point(4, 213);
+            this.SubmitButton.Margin = new System.Windows.Forms.Padding(4);
             this.SubmitButton.Name = "SubmitButton";
-            this.SubmitButton.Size = new System.Drawing.Size(148, 45);
+            this.SubmitButton.Size = new System.Drawing.Size(197, 55);
             this.SubmitButton.TabIndex = 12;
             this.SubmitButton.Text = "Submit";
             this.SubmitButton.UseVisualStyleBackColor = false;
@@ -109,28 +118,50 @@
             this.CancelButton.Font = new System.Drawing.Font("Candara", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.CancelButton.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
             this.CancelButton.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.CancelButton.Location = new System.Drawing.Point(152, 173);
+            this.CancelButton.Location = new System.Drawing.Point(203, 213);
+            this.CancelButton.Margin = new System.Windows.Forms.Padding(4);
             this.CancelButton.Name = "CancelButton";
-            this.CancelButton.Size = new System.Drawing.Size(148, 45);
+            this.CancelButton.Size = new System.Drawing.Size(197, 55);
             this.CancelButton.TabIndex = 13;
             this.CancelButton.Text = "Cancel";
             this.CancelButton.UseVisualStyleBackColor = false;
             this.CancelButton.Click += new System.EventHandler(this.CancelButton_Click);
             // 
+            // TableName
+            // 
+            this.TableName.Location = new System.Drawing.Point(127, 93);
+            this.TableName.Margin = new System.Windows.Forms.Padding(4);
+            this.TableName.Name = "TableName";
+            this.TableName.Size = new System.Drawing.Size(200, 22);
+            this.TableName.TabIndex = 14;
+            this.TableName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Location = new System.Drawing.Point(30, 101);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(84, 15);
+            this.textBox2.TabIndex = 15;
+            this.textBox2.Text = "TableName";
+            // 
             // PostForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.ClientSize = new System.Drawing.Size(304, 223);
+            this.ClientSize = new System.Drawing.Size(405, 274);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.TableName);
             this.Controls.Add(this.CancelButton);
             this.Controls.Add(this.SubmitButton);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.DataBox);
-            this.Controls.Add(this.TargetTableBox);
+            this.Controls.Add(this.TableColumn);
             this.Controls.Add(this.FormHeader);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "PostForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "PostForm";
@@ -142,11 +173,13 @@
         #endregion
 
         private System.Windows.Forms.Panel FormHeader;
-        private System.Windows.Forms.TextBox TargetTableBox;
+        private System.Windows.Forms.TextBox TableColumn;
         private System.Windows.Forms.TextBox DataBox;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button SubmitButton;
         private System.Windows.Forms.Button CancelButton;
+        private System.Windows.Forms.TextBox TableName;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
